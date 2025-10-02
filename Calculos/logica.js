884a939f140936fd77fc0616f12f0f15e9926bc5
jvzin <joao.v.silva1008@aluno.senai.br>
2025-10-02 16:52:06 -0300

    Add files via upload

--- Calculos/logica.js
+++ Calculos/logica.js
@@ -1,20 +1,28 @@
-function soma(){
+function soma() {
     var numero1 = document.getElementById("n1").valueAsNumber
     var numero2 = document.getElementById("n2").valueAsNumber
     var resposta = document.getElementById("respSoma")
 
     resposta.textContent = numero1 + numero2
-
-    // console.log(numero1, numero2)
 }
 
-function sub(){
- 
-    var numero3 = document.getElementById("n3").valueAsNumber
-    var numero4 = document.getElementById("n4").valueAsNumber
-    var resposta = document.getElementById("respSub")
+function sub() {
 
-    resposta.textContent = numero3 - numero4
+    var num3 = parseFloat(document.getElementById("n3").value)
+    var num4 = parseFloat(document.getElementById("n4").value)
+    document.getElementById("respSub").textContent = num3 - num4
+}
 
-    // console.log(numero3, numero4)
+function mult() {
+
+    var number5 = parseFloat(document.getElementById("n5").value)
+    var number6 = parseFloat(document.getElementById("n6").value)
+    document.getElementById("respMult").textContent = number5 * number6
+}
+
+function div() {
+
+    var numeral7 = parseFloat(document.getElementById("n7").value)
+    var numeral8 = parseFloat(document.getElementById("n8").value)
+    document.getElementById("respDiv").textContent = numeral7 / numeral8
 }
--- README.md
+++ README.md
@@ -6,5 +6,15 @@ label- escreva do portugol <br>
 hl - escreva titulo do portugol <br>
 input - leia do portugol <br>
 br - \n do portugol 
+valueAsNumber e parseFloat são iguais 
 # git 
 git clone "nome do repositório"
+
+# modificações do site
+ background-color: #090909;
+    color: #1a03e7;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
